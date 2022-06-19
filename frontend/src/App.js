73eb04d161555900ccb68dfac13a5b943b1fd5f4
Ng Chee Wee <ng_cheewee@yahoo.com>
2022-06-19 16:49:55 +0800

    lesson3-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">amanzona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
