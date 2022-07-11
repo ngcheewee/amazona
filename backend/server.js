414dfa49cf7956832b4b6b801af202bc344d4f90
Ng Chee Wee <ng_cheewee@yahoo.com>
2022-07-11 16:25:59 +0800

    Video-33-Public To Heroku

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
